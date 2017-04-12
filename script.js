f692822761baf1885405ff9dd27a2ba54174654d
Sol Laudon <blaudon1@gmail.com>
2017-04-12 16:22:33 -0600

    second board

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 var body=document.getElementsByTagName('body')[0];
 
+// for (i = 1; i <= 81; i++) {
+//
+//     var tile=document.createElement('div');
+//
+//     tile.style.width="11.1%";
+//     tile.style.float="left";
+//     tile.style.paddingBottom="11.1%";
+//
+//     if (i % 2 === 0 ) {
+//       tile.style.backgroundColor="black";
+//       body.appendChild(tile);
+//     } else {
+//       tile.style.backgroundColor="red";
+//       body.appendChild(tile);
+//     }
+// }
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 for (i = 1; i <= 81; i++) {
 
     var tile=document.createElement('div');
@@ -7,12 +33,7 @@ for (i = 1; i <= 81; i++) {
     tile.style.width="11.1%";
     tile.style.float="left";
     tile.style.paddingBottom="11.1%";
+    tile.style.backgroundColor=getRandomColor();
 
-    if (i % 2 === 0 ) {
-      tile.style.backgroundColor="black";
-      body.appendChild(tile);
-    } else {
-      tile.style.backgroundColor="red";
-      body.appendChild(tile);
-    }
+    body.appendChild(tile);
 }
